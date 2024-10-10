da5a37c0823225c801d54c88ac77f840b926d0d0
estagiario.1 <fabioallexx16@hotmail.com>
2024-10-10 16:20:54 +0100

    PARA VER AMANHÃ

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
 from register import views as v
-from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
@@ -10,6 +9,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', v.register, name="register"),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
