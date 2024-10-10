32de2459982cee79306c8b89f151ab19fe4aa899
estagiario.1 <fabioallexx16@hotmail.com>
2024-10-10 11:17:30 +0100

    Sidenav interativa e logout funcional

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,5 +128,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_TEMPLATE_PACK="bootstrap4"
 
-LOGIN_REDIRECT_URL = "/"
-LOGOUT_REDIRECT_URL = "/"
+LOGIN_REDIRECT_URL = '/home'
+LOGOUT_REDIRECT_URL = '/login'
--- mysite/urls.py
+++ mysite/urls.py
@@ -3,8 +3,8 @@ from django.urls import include, path
 from register import views as v
 
 urlpatterns = [
-    path('', include('main.urls')), # main will be the name of your app
+    path('', include('main.urls')),
+    path('', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
     path('register/', v.register, name="register"),
-    path('', include("django.contrib.auth.urls")),
 ]
